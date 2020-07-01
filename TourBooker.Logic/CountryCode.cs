84bce49dc354723e119fcb4b06f292aaa6165be4
Vasilis Dimitriou <v.dimitriou@dataverse.gr>
2020-07-01 15:47:09 +0300

    Added CountryCode class.

---
+++ TourBooker.Logic/CountryCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourBooker.Logic
+{
+    public class CountryCode
+    {
+        public string Value { get; }
+
+        public CountryCode(string value)
+        {
+            Value = value;
+        }
+
+        public override string ToString() => Value;
+    }
+}
