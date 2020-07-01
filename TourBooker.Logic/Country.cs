43c71d7bb32f1a80cffb6fc7023a0d9693e299b0
Vasilis Dimitriou <v.dimitriou@dataverse.gr>
2020-07-01 11:11:36 +0300

    Add project files.

---
+++ TourBooker.Logic/Country.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pluralsight.AdvCShColls.TourBooker.Logic
+{
+	public class Country
+	{
+		public string Name { get; }
+		public string Code { get; }
+		public string Region { get; }
+		public int Population { get; }
+		public Country(string name, string code, string region, int population)
+		{
+			Name = name;
+			Code = code;
+			Region = region;
+			Population = population;
+		}
+        public override string ToString() => $"{Name} ({Code})";
+    }
+}
