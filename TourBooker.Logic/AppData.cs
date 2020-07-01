a88eb64998e0306e5b318d9f8adf3f6725876715
Vasilis Dimitriou <v.dimitriou@dataverse.gr>
2020-07-01 15:43:44 +0300

    Used a Dictionary.

--- TourBooker.Logic/AppData.cs
+++ TourBooker.Logic/AppData.cs
@@ -9,13 +9,12 @@ namespace Pluralsight.AdvCShColls.TourBooker.Logic
 	public class AppData
 	{
 		public List<Country> AllCountries { get; private set; }
-		public SortedList<string, Country> AllCountriesByKey { get; private set; }
+		public Dictionary<string, Country> AllCountriesByKey { get; private set; }
 		public void Initialize(string csvFilePath)
 		{
 			CsvReader reader = new CsvReader(csvFilePath);
 			AllCountries = reader.ReadAllCountries().OrderBy(c => c.Name).ToList();
-			var dict = AllCountries.ToDictionary(c => c.Code, StringComparer.OrdinalIgnoreCase);
-			AllCountriesByKey = new SortedList<string, Country>(dict);
+			AllCountriesByKey = AllCountries.ToDictionary(c => c.Code, StringComparer.OrdinalIgnoreCase);
 		}
 	}
 }
