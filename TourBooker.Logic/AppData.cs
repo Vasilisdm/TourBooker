efcda6c5e8ef1547792f23392a5a3c8cd205b8b9
vasilisdimitriou <vasilisdmr@gmail.com>
2020-07-16 06:43:52 +0300

    Changed queue to concurrent queue.

--- TourBooker.Logic/AppData.cs
+++ TourBooker.Logic/AppData.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,8 +13,8 @@ namespace Pluralsight.AdvCShColls.TourBooker.Logic
 		public Dictionary<CountryCode, Country> AllCountriesByKey { get; private set; }
 		public List<Customer> Customers { get; private set; }
 			 = new List<Customer>() { new Customer("Simon"), new Customer("Kim") };
-		public Queue<(Customer TheCustomer, Tour TheTour)> BookingRequests { get; }
-			= new Queue<(Customer, Tour)>();
+		public ConcurrentQueue<(Customer TheCustomer, Tour TheTour)> BookingRequests { get; }
+			= new ConcurrentQueue<(Customer, Tour)>();
 		public LinkedList<Country> ItineraryBuilder { get; } = new LinkedList<Country>();
 		public SortedDictionary<string, Tour> AllTours { get; private set; } 
 			= new SortedDictionary<string, Tour>();
